bcbbffda836affbb19f33b93eb824272130ef88f
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-03-23 17:33:50 +0000

    pretty print move

--- move.go
+++ move.go
@@ -1,5 +1,9 @@
 package checkers
 
+import (
+	"fmt"
+)
+
 var NO_JUMP = Coordinate{-1, -1}
 
 type Move struct {
@@ -10,4 +14,20 @@ type Move struct {
 
 func NewMove(from Coordinate, to Coordinate, jump Coordinate) Move {
 	return Move{From: from, To: to, Jump: jump}
+}
+
+func (move Move) String() string {
+	from := move.From
+	to := move.To
+	jump := move.Jump
+
+	if move.Jump == NO_JUMP {
+		return fmt.Sprintf("from[%v] jump[NONE] to[%v]", from, to)
+	} else {
+		return fmt.Sprintf("from[%v] jump[%v] to[%v]", from, jump, to)
+	}
+}
+
+func (move Move) GoString() string {
+	return move.String()
 }
